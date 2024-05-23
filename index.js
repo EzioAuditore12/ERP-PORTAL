17a52272a957df13c7acce916e683a88e066548d
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-05-23 19:59:03 +0530

    -a

--- index.js
+++ index.js
@@ -5,5 +5,25 @@ const app = express();
 
 app.use('./public')
 
+app.use(express.json())
+
+app.post('/register',(req,res)=>{
+    const{username,password} = req.body
+    const id=`${Date.now()}`
+
+    const user={
+        id,
+        username,
+        password
+    }
+
+    userstore[id]=user
+
+    console.log(`Registeration is sucessful${userstore[id]}`)
+
+    return res.json({id})
+})
+
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
