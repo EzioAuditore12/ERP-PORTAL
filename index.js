514aed8a007f69abc8d8e4856bc5d2ec2b083314
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-05-23 20:33:22 +0530

    -a

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = 3000;
 const app = express();
 
-app.use('./public')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-app.use(express.json())
+const userstore = {}; // Define the userstore object
 
-app.post('/register',(req,res)=>{
-    const{username,password} = req.body
-    const id=`${Date.now()}`
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    const id = `user_${Date.now()}`;
 
-    const user={
+    const user = {
         id,
         username,
         password
-    }
+    };
 
-    userstore[id]=user
+    userstore[id] = user;
 
-    console.log(`Registeration is sucessful${userstore[id]}`)
+    console.log(`Registration is successful: ${JSON.stringify(userstore[id])}`);
 
-    return res.json({id})
-})
-
+    return res.json({ success: true, id });
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
