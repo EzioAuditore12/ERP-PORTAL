31f0eeae6a1dcd94875c52610f7007420a3b0470
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-05-23 21:43:17 +0530

    -a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const{generateAuthenticationOptions}=require('@simplewebauthn/server')
 
 const PORT = 3000;
 const app = express();
